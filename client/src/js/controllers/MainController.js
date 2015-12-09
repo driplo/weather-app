c1c07f01b5b6e3da74d0ac63d4cc2d6624be411c
Adrien AUER <draydrayz@gmail.com>
2015-12-09 22:40:23 +0100

    added langage support

--- client/src/js/controllers/MainController.js
+++ client/src/js/controllers/MainController.js
@@ -2,17 +2,19 @@ app.controller('MainController', ['$scope', 'forecast', '$http', 'apiKey', '$san
 
   navigator.geolocation.getCurrentPosition(function(position) {
     console.log(position);
+    var lang = navigator.language;
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
 
     $scope.$apply(function(){
       $scope.lat = lat;
       $scope.lng = lng;
+      $scope.lang = lang;
     });
 
     $http({
       method: 'GET',
-      url: 'http://api.openweathermap.org/data/2.5/weather?lat='+$scope.lat+'&lon='+$scope.lng+'&units=metric&lang=fr&appid=' + apiKey
+      url: 'http://api.openweathermap.org/data/2.5/weather?lat='+$scope.lat+'&lon='+$scope.lng+'&units=metric&lang='+$scope.lang+'&appid=' + apiKey
     }).success(function(dataW) {
       $scope.currentWeather = dataW;
       console.log($scope.currentWeather);
