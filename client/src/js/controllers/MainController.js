82a0bc372075ad834c58018c7e77e2c3bd5f611d
Adrien AUER <draydrayz@gmail.com>
2015-12-09 22:27:04 +0100

    feat: added geolocation detection

--- client/src/js/controllers/MainController.js
+++ client/src/js/controllers/MainController.js
@@ -1,9 +1,29 @@
-app.controller('MainController', ['$scope', 'forecast', 'currentWeather', '$http', 'apiKey', '$sanitize', function($scope, forecast, currentWeather, $http, apiKey, $sanitize) {
+app.controller('MainController', ['$scope', 'forecast', '$http', 'apiKey', '$sanitize', function($scope, forecast, $http, apiKey, $sanitize) {
 
-  currentWeather.success(function (dataW) {
-    $scope.currentWeather = dataW;
-    console.log($scope.currentWeather);
-  });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    $scope.$apply(function(){
+      $scope.lat = lat;
+      $scope.lng = lng;
+    });
+
+    $http({
+      method: 'GET',
+      url: 'http://api.openweathermap.org/data/2.5/weather?lat='+$scope.lat+'&lon='+$scope.lng+'&units=metric&lang=fr&appid=' + apiKey
+    }).success(function(dataW) {
+      $scope.currentWeather = dataW;
+      console.log($scope.currentWeather);
+    });
+
+  })
+
+  // currentWeather.success(function (dataW) {
+  //   $scope.currentWeather = dataW;
+  //   console.log($scope.currentWeather);
+  // });
 
   forecast.success(function (dataFC) {
     $scope.forecast = dataFC.list;
